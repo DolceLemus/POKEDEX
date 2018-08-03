da49048a92b8999d8bcce667d2248599884e8ea8
DolceLemus <dulce.lemus.lara@gmail.com>
2018-08-02 23:41:39 -0500

    una voz del mas allá lee la descripcion

--- js/app.js
+++ js/app.js
@@ -1,51 +1,21 @@
-// const descriptionContainer = document.getElementById('description-container');
-// const btnSearch = document.getElementById('btn-search');
-
-// let data = null;
-
-// const pokemonRequest = new XMLHttpRequest();
-
-// pokemonRequest.addEventListener("readystatechange", function () {
-//     if (pokemonRequest.readyState === pokemonRequest.DONE) {
-//         data = JSON.parse(pokemonRequest.responseText);
-//         console.log (data);
-//     } else(
-//         console.log("cargando")
-//     )
-// });
-
-// const getImage = function () {
-//     data.sprites.keyword;
-// }
-
-
-// pokemonRequest.open("GET",`https://pokeapi.co/api/v2/pokemon/${keyword}`);
-// pokemonRequest.send();
-
-
-// let connectionObject = {
-//     "url": "https://pokeapi.co/api/v2/pokemon-species/1/",
-//     "async": true,
-//     "crossDomain":true,
-//     "method": "GET",
-//     "headers":{}
-// }
-
-// $.ajax(connectionObject).done(function(response){
-//     console.log(JSON.parse(response));
-// })
-
-
 
 const getPokemon = function (ev) {
   ev.preventDefault();
   const number = $("#keyword").val();
   const getURL = "https://pokeapi.co/api/v2/pokemon/";
+  const getURLDesc = "https://pokeapi.co/api/v1/description/";
+
   $.get(getURL + number,  function(pokemon) {
       console.log(pokemon);
-    $("#name").text(pokemon.name);
+    $("#name").text("Name: " + pokemon.name);
     $("#image").attr("src",pokemon.sprites.front_default);
   });
+
+  $.get(getURLDesc + number,  function(pokemon) {
+  $("#description").text("Description: " + pokemon.description);
+    //HABLA jajajaja
+  $("btn-search").click(responsiveVoice.speak(pokemon.description));
+});
 }
 
 $("#site").submit(getPokemon); 
@@ -57,3 +27,15 @@ $("#site").submit(getPokemon);
 // https://api.jquery.com/submit/
 // https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Utilizando_Fetch
 // https://github.com/PokeAPI/sprites/tree/master/sprites
+
+
+// <<<<--------------- $.get ------------------>>>>>
+// jQuery.get( url [, data ] [, success ] [, dataType ] ) 
+// This is a shorthand Ajax function, which is equivalent to:
+// $.ajax({
+//     url: url,
+//     data: data,
+//     success: success,
+//     dataType: dataType
+//   });
+// https://api.jquery.com/jquery.get/
