c75c588dc4f6ef776de7cb25e00d67878348d32e
DolceLemus <dulce.lemus.lara@gmail.com>
2018-08-06 17:41:04 -0500

    all.js funciona con fetch

---
+++ js/all.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+     const url = 'https://pokeapi.co/api/v2/pokedex/1';
+     const template = function (name, picture) {
+        var t = "<div class='element'><img src='" + picture + "'/><p>'" + name + "'</p></div>"
+        return t;
+      }
+
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            console.log(data.pokemon_entries);
+            let ul = document.getElementById('list');
+            let pokemones = data.pokemon_entries;
+            pokemones.forEach(element => {
+                let picture = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + element.entry_number + '.png'; 
+                let name = element.pokemon_species.name;
+                $('#list').append(template(name,picture));
+            });
+        })
+        .catch(function (error) {
+            console.log(JSON.stringify(error));
+        });   
+  
+
+})
+
+
+// source
+// fetch(url) // Call the fetch function passing the url of the API as a parameter
+// .then(function() {
+//     // Your code for handling the data you get from the API
+// })
+// .catch(function() {
+//     // This is where you run code if the server returns any errors
+// });
+
+
+// const number = (min, max) => {
+    //     return Math.floor(Math.random() * (max - min)) + min;
+    //   }
+
+    //   console.log(number(1,900));
--- js/app.js
+++ js/app.js
@@ -1,38 +1,46 @@
 
-const getPokemon = function (ev) {
-  ev.preventDefault();
-  const number = $("#keyword").val();
-  const getURL = "https://pokeapi.co/api/v2/pokemon/";
-  const getURLLocat = "https://pokeapi.co/api/v2/location/";
-  const getURLAbil = "https://pokeapi.co/api/v2/ability/";
+
+
+  const getPokemon = function (ev) {
+    ev.preventDefault();
+    const number = $("#keyword").val();
+    const getURL = "https://pokeapi.co/api/v2/pokemon/";
+    const getURLLocat = "https://pokeapi.co/api/v2/location/";
+    const getURLAbil = "https://pokeapi.co/api/v2/ability/";
 
 
 
-  $.get(getURL + number, function (data) {
-    console.log(data);
-    $("#name").text("Name: " + data.name);
-    $("#image").attr("src", data.sprites.front_shiny);
-    $("#basic-exp").text("Exp.:" + data.base_experience);
-    $("#weight").text("Weight:" + data.weight);
-  });
+    $.get(getURL + number, function (data) {
+      console.log(data);
+      $("#name").text("Name: " + data.name);
+      $("#image").attr("src", data.sprites.front_shiny);
+      $("#basic-exp").text("Exp.:" + data.base_experience);
+      $("#weight").text("Weight:" + data.weight);
+    });
 
- 
 
-  $.get(getURLLocat + number, function (data) {
-    console.log(data);
-    $("#region").text("Region: " + data.region.name);
-  });
 
-  $.get(getURLAbil + number, function (data) {
-    console.log(data);
-    $("#short-ability").text("Ability: " + data.effect_entries[0].effect);
-    $("#ability").text("Ability: " + data.effect_entries[0].short_effect);
+    $.get(getURLLocat + number, function (data) {
+      console.log(data);
+      $("#region").text("Region: " + data.region.name);
+    });
+
+    $.get(getURLAbil + number, function (data) {
+      console.log(data);
+      $("#short-ability").text("Ability: " + data.effect_entries[0].effect);
+      $("#ability").text("Ability: " + data.effect_entries[0].short_effect);
       //HABLA jajajaja
       $("btn-search").click(responsiveVoice.speak(data.effect_entries[0].effect));
-  });
-}
+    });
 
-$("#site").submit(getPokemon); 
+
+    const getURLpokedex = "https://pokeapi.co/api/v2/pokedex/";
+    $.get(getURLpokedex , function (data) {
+      console.log(data);
+    });
+  }
+
+  $("#site").submit(getPokemon);
 
 
 // sources
